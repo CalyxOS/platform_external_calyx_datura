e8ea06a9ee6bf6ae481072abe9435a7e7f2342ed
Oliver Scott <olivercscott@gmail.com>
2021-05-14 20:20:27 -0400

    Add packages from work profiles to the list of installed applications
    Issue: calyxos#457
    Change-Id: Ib4f745c84c82186ccc3481e7433d60f4abd66968
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/org/calyxos/datura/utils/CommonUtils.kt
+++ app/src/main/java/org/calyxos/datura/utils/CommonUtils.kt
@@ -11,7 +11,11 @@ import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.ApplicationInfoFlags
+import android.content.pm.UserInfo
 import android.os.Process
+import android.os.UserHandle
+import android.os.UserManager
 import androidx.core.graphics.drawable.toBitmap
 import org.calyxos.datura.R
 import org.calyxos.datura.models.App
@@ -39,20 +43,27 @@ object CommonUtils {
         val packageManager = context.packageManager
 
         val usageStatsList = getUsageStats(context)
-        val packageList = packageManager.getInstalledPackages(
-            PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong())
-        ).filter { Process.isApplicationUid(it.applicationInfo.uid) }
+        val packageList = getAppsInstalledForAllUsers(context)
+            .filter { Process.isApplicationUid(it.uid) }
 
-        packageList.forEach { packageInfo ->
+        packageList.forEach {
+            val pkgInfo = packageManager.getPackageInfo(
+                it.packageName,
+                PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong())
+            )
             val app = App(
-                packageInfo.applicationInfo.loadLabel(packageManager).toString(),
-                packageInfo.packageName,
-                packageInfo.applicationInfo.loadIcon(packageManager).toBitmap(96, 96),
-                packageInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM != 0,
-                packageInfo.applicationInfo.uid,
-                packageInfo.requestedPermissions?.contains(Manifest.permission.INTERNET) ?: false,
+                it.loadLabel(packageManager).toString(),
+                it.packageName,
+                packageManager.getUserBadgedIcon(
+                    it.loadIcon(packageManager),
+                    UserHandle.getUserHandleForUid(it.uid)
+                ).toBitmap(96, 96),
+                it.flags and ApplicationInfo.FLAG_SYSTEM != 0,
+                it.uid,
+                pkgInfo.requestedPermissions?.contains(Manifest.permission.INTERNET) ?: false,
                 false,
-                usageStatsList.firstOrNull { it.packageName == packageInfo.packageName }?.lastTimeUsed ?: 0L
+                usageStatsList.firstOrNull { u -> u.packageName == it.packageName }?.lastTimeUsed
+                    ?: 0L
             )
             applicationList.add(app)
         }
@@ -74,5 +85,53 @@ object CommonUtils {
             startTime,
             endTime
         )
+    }
+
+    // TODO: Drop this and use ASfP or something else that doesn't requires reflection
+    private fun getAppsInstalledForAllUsers(context: Context): List<ApplicationInfo> {
+        val packages = mutableListOf<ApplicationInfo>()
+        val userManager = context.getSystemService(UserManager::class.java)
+        userManager.getProfiles(getMyUserId()).forEach {
+            packages.addAll(
+                context.packageManager.getInstalledApplicationsAsUser(
+                    ApplicationInfoFlags.of(PackageManager.GET_META_DATA.toLong()),
+                    it.id
+                )
+            )
+        }
+        return packages
+    }
+
+    private fun getMyUserId(): Int {
+        val types = arrayOf(
+            Int::class.javaPrimitiveType
+        )
+        val method = UserHandle::class.java.getMethod("getUserId", *types)
+        val response = method.invoke(this, Process.myUid())
+        return response as Int
+    }
+
+    private fun UserManager.getProfiles(
+        userId: Int
+    ): List<UserInfo> {
+        val types = arrayOf(
+            Int::class.javaPrimitiveType
+        )
+        val method = this.javaClass.getMethod("userId", *types)
+        val response = method.invoke(this, userId)
+        return response as List<UserInfo>
+    }
+
+    private fun PackageManager.getInstalledApplicationsAsUser(
+        flags: ApplicationInfoFlags,
+        userId: Int
+    ): List<ApplicationInfo> {
+        val types = arrayOf(
+            ApplicationInfoFlags::class.java,
+            Int::class.javaPrimitiveType
+        )
+        val method = this.javaClass.getMethod("getInstalledApplicationsAsUser", *types)
+        val response = method.invoke(this, flags, userId)
+        return response as List<ApplicationInfo>
     }
 }
