CalyxOS/platform_packages_apps_Datura

AppListFragment: Allow users to sort list by name or last usage
Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
Change-Id: I6ea62efd223d6c33483becf58cdd85e10f107d90

app/src/main/java/org/calyxos/datura/applist/AppListFragment.kt
@@ -20,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import org.calyxos.datura.R
 import org.calyxos.datura.main.MainActivityViewModel
+import org.calyxos.datura.utils.Sort
 import javax.inject.Inject
 
 @AndroidEntryPoint(Fragment::class)
@@ -40,7 +41,9 @@ class AppListFragment : Hilt_AppListFragment(R.layout.fragment_app_list) {
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.appList.collect { appListRVAdapter.submitList(it) }
         }
-        view.findViewById<RecyclerView>(R.id.recyclerView).adapter = appListRVAdapter
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView).apply {
+            adapter = appListRVAdapter
+        }
 
         // Search View
         val searchView = view.findViewById<SearchView>(R.id.searchView)
@@ -49,8 +52,23 @@ class AppListFragment : Hilt_AppListFragment(R.layout.fragment_app_list) {
         searchBar.apply {
             inflateMenu(R.menu.menu_main)
             setOnMenuItemClickListener {
-                if (it.itemId == R.id.about) {
-                    findNavController().navigate(R.id.aboutFragment)
+                when (it.itemId) {
+                    R.id.about -> {
+                        findNavController().navigate(R.id.aboutFragment)
+                    }
+                    R.id.sort -> {
+                        if (viewModel.sort == Sort.NAME) {
+                            viewModel.sortAppList(Sort.LAST_USED)
+                            recyclerView.post { recyclerView.scrollToPosition(0) }
+                            it.title = getString(R.string.sort_by_name)
+                            viewModel.sort = Sort.LAST_USED
+                        } else {
+                            viewModel.sortAppList(Sort.NAME)
+                            recyclerView.post { recyclerView.scrollToPosition(0) }
+                            it.title = getString(R.string.sort_by_last_used)
+                            viewModel.sort = Sort.NAME
+                        }
+                    }
                 }
                 true
             }

app/src/main/java/org/calyxos/datura/main/MainActivityViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.calyxos.datura.models.App
 import org.calyxos.datura.utils.CommonUtils
+import org.calyxos.datura.utils.Sort
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,6 +22,8 @@ import javax.inject.Inject
 class MainActivityViewModel @Inject constructor(
     @ApplicationContext private val context: Context
 ) : ViewModel() {
+
+    var sort = Sort.NAME
 
     private val _appList = MutableStateFlow(emptyList<App>())
     val appList = _appList.asStateFlow()
@@ -38,6 +41,15 @@ class MainActivityViewModel @Inject constructor(
             _appList.value.filter { it.name.contains(text, true) }
         } else {
             emptyList()
+        }
+    }
+
+    fun sortAppList(sort: Sort) {
+        if (sort == Sort.LAST_USED) {
+            _appList.value = _appList.value.sortedByDescending { a -> a.lastTimeUsed }
+        } else {
+            _appList.value =
+                _appList.value.sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it.name })
         }
     }
 }

app/src/main/java/org/calyxos/datura/utils/Sort.kt
@@ -1,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.utils
+
+enum class Sort {
+    LAST_USED,
+    NAME
+}

CalyxOS/platform_external_calyx_datura

Co-authored-by: Ese Udom <udomese@yahoo.com>

app/src/main/java/org/calyxos/datura/applist/AppListFragment.kt
@@ -20,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import org.calyxos.datura.R
 import org.calyxos.datura.main.MainActivityViewModel
+import org.calyxos.datura.models.Sort
 import javax.inject.Inject
 
 @AndroidEntryPoint(Fragment::class)
@@ -40,7 +41,9 @@ class AppListFragment : Hilt_AppListFragment(R.layout.fragment_app_list) {
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.appList.collect { appListRVAdapter.submitList(it) }
         }
-        view.findViewById<RecyclerView>(R.id.recyclerView).adapter = appListRVAdapter
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView).apply {
+            adapter = appListRVAdapter
+        }
 
         // Search View
         val searchView = view.findViewById<SearchView>(R.id.searchView)
@@ -49,8 +52,23 @@ class AppListFragment : Hilt_AppListFragment(R.layout.fragment_app_list) {
         searchBar.apply {
             inflateMenu(R.menu.menu_main)
             setOnMenuItemClickListener {
-                if (it.itemId == R.id.about) {
-                    findNavController().navigate(R.id.aboutFragment)
+                when (it.itemId) {
+                    R.id.about -> {
+                        findNavController().navigate(R.id.aboutFragment)
+                    }
+                    R.id.sort -> {
+                        if (viewModel.sort == Sort.NAME) {
+                            viewModel.sortAppList(Sort.LAST_USED)
+                            recyclerView.post { recyclerView.scrollToPosition(0) }
+                            it.title = getString(R.string.sort_by_name)
+                            viewModel.sort = Sort.LAST_USED
+                        } else {
+                            viewModel.sortAppList(Sort.NAME)
+                            recyclerView.post { recyclerView.scrollToPosition(0) }
+                            it.title = getString(R.string.sort_by_last_used)
+                            viewModel.sort = Sort.NAME
+                        }
+                    }
                 }
                 true
             }

app/src/main/java/org/calyxos/datura/main/MainActivityViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import org.calyxos.datura.models.App
 import org.calyxos.datura.utils.CommonUtils
+import org.calyxos.datura.models.Sort
 import javax.inject.Inject
 
 @HiltViewModel
@@ -21,6 +22,8 @@ import javax.inject.Inject
 class MainActivityViewModel @Inject constructor(
     @ApplicationContext private val context: Context
 ) : ViewModel() {
+
+    var sort = Sort.NAME
 
     private val _appList = MutableStateFlow(emptyList<App>())
     val appList = _appList.asStateFlow()
@@ -38,6 +41,15 @@ class MainActivityViewModel @Inject constructor(
             _appList.value.filter { it.name.contains(text, true) }
         } else {
             emptyList()
+        }
+    }
+
+    fun sortAppList(sort: Sort) {
+        if (sort == Sort.LAST_USED) {
+            _appList.value = _appList.value.sortedByDescending { a -> a.lastTimeUsed }
+        } else {
+            _appList.value =
+                _appList.value.sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER) { it.name })
         }
     }
 }

app/src/main/java/org/calyxos/datura/models/Sort.kt
@@ -1,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.models
+
+enum class Sort {
+    LAST_USED,
+    NAME
+}