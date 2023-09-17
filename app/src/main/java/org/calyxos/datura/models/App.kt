CalyxOS/platform_packages_apps_Datura

AppListRVAdapter: Insert headers for installed and system apps
Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
Change-Id: I894ce42b4833b8f3eb20809169de638c530be95b

app/src/main/java/org/calyxos/datura/models/App.kt
@@ -15,5 +15,6 @@ data class App(
     val uid: Int = -1,
     val requestsInternetPermission: Boolean = false,
     var isExpanded: Boolean = false,
-    val lastTimeUsed: Long = 0
-)
+    val lastTimeUsed: Long = 0,
+    override val type: DaturaItemType = DaturaItemType.ITEM
+) : DaturaItem

app/src/main/java/org/calyxos/datura/models/DaturaItem.kt
@@ -1,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.models
+
+interface DaturaItem {
+    val type: DaturaItemType
+}

app/src/main/java/org/calyxos/datura/models/Header.kt
@@ -1,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.models
+
+data class Header(
+    val name: String = String(),
+    override val type: DaturaItemType = DaturaItemType.HEADER
+) : DaturaItem

CalyxOS/platform_external_calyx_datura

app/src/main/java/org/calyxos/datura/models/App.kt
@@ -15,5 +15,6 @@ data class App(
     val uid: Int = -1,
     val requestsInternetPermission: Boolean = false,
     var isExpanded: Boolean = false,
-    val lastTimeUsed: Long = 0
-)
+    val lastTimeUsed: Long = 0,
+    override val type: Type = Type.APP
+) : DaturaItem

app/src/main/java/org/calyxos/datura/models/DaturaItem.kt
@@ -1,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.models
+
+interface DaturaItem {
+    val type: Type
+}

app/src/main/java/org/calyxos/datura/models/Header.kt
@@ -1,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The Calyx Institute
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.calyxos.datura.models
+
+data class Header(
+    val name: String = String(),
+    override val type: Type = Type.HEADER
+) : DaturaItem