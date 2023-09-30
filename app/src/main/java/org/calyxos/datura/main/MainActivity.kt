060468563f4642d531f2c15595b9e32c92f6c286
Aayush Gupta <aayushgupta219@gmail.com>
2023-09-30 16:42:16 +0530

    MainActivity: Ask user to open datura in personal profile when required
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I7a966c32abfffc25261be1ba9952504a3e8fac00

--- app/src/main/java/org/calyxos/datura/main/MainActivity.kt
+++ app/src/main/java/org/calyxos/datura/main/MainActivity.kt
@@ -6,7 +6,10 @@
 package org.calyxos.datura.main
 
 import android.os.Bundle
+import android.os.UserManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import org.calyxos.datura.R
 
@@ -15,5 +18,22 @@ class MainActivity : Hilt_MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (getSystemService(UserManager::class.java).isManagedProfile) {
+            navigateToWorkFragment()
+        }
+    }
+
+    private fun navigateToWorkFragment() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val navOptions = NavOptions.Builder()
+            .setPopUpTo(R.id.navigation_resource, true)
+            .build()
+        navOptions.shouldLaunchSingleTop()
+
+        navController.navigate(R.id.workFragment, null, navOptions)
     }
 }
