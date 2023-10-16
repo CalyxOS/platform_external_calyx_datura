81581254dddc748dd948e10f3fc10bbed868c581
Aayush Gupta <aayushgupta219@gmail.com>
2023-10-16 17:17:54 +0530

    gradle: Bump dependencies to latest stable version
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Id380b85d1a53730abc11ab014d5de9f7b78fed60

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,24 +74,24 @@ dependencies {
 
     compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
     // Navigation Components
-    val navVersion = "2.5.3"
+    val navVersion = "2.7.4"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     // Hilt
-    val hiltVersion = "2.45"
+    val hiltVersion = "2.48.1"
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.8.20" apply false
-    id("com.google.dagger.hilt.android") version "2.45" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.5.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.10" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
 }
