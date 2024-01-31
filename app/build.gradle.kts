c58b32811637d8eeeccbcfea559017793bbc8144
Aayush Gupta <aayushgupta219@gmail.com>
2024-01-31 07:02:41 +0100

    Bump AGP, Gradle and dependencies to latest stable release
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I7737b00af0b93e778665ca0f2c9e28ec87bc29a9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,22 +72,22 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // ViewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
 
     // Navigation Components
-    val navVersion = "2.7.4"
+    val navVersion = "2.7.6"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     // Hilt
-    val hiltVersion = "2.48.1"
+    val hiltVersion = "2.50"
     ksp("com.google.dagger:hilt-compiler:$hiltVersion")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.6.1" apply false
 }
