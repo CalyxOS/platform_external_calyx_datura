97fb394ebb0192c83129067fbb5f02de1ad64f8f
Oliver Scott <olivercscott@gmail.com>
2024-04-18 20:06:21 -0400

    Copy Restrict cleartext network traffic setting
    Issue: calyxos#1952
    Change-Id: I87406a965117230008f261f860b5f0bc63e0fb28
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- app/src/main/java/org/calyxos/datura/main/MainActivity.kt
+++ app/src/main/java/org/calyxos/datura/main/MainActivity.kt
@@ -5,8 +5,14 @@
 
 package org.calyxos.datura.main
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.os.UserManager
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
@@ -16,6 +22,22 @@ import org.calyxos.datura.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private val viewModel: MainActivityViewModel by viewModels()
+
+    private val packageReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (context != null && intent != null) {
+                when (intent.action) {
+                    Intent.ACTION_PACKAGE_ADDED,
+                    Intent.ACTION_PACKAGE_REMOVED -> viewModel.fetchAppList()
+                    else -> Log.i(TAG, "Got an unhandled action")
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DynamicColors.applyToActivityIfAvailable(this)
@@ -24,6 +46,22 @@ class MainActivity : AppCompatActivity() {
         if (getSystemService(UserManager::class.java)?.isManagedProfile == true) {
             navigateToWorkFragment()
         }
+
+        registerReceiverForAllUsers(
+            packageReceiver,
+            IntentFilter().apply {
+                addAction(Intent.ACTION_PACKAGE_ADDED)
+                addAction(Intent.ACTION_PACKAGE_REMOVED)
+                addDataScheme("package")
+            },
+            null,
+            null
+        )
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(packageReceiver)
+        super.onDestroy()
     }
 
     private fun navigateToWorkFragment() {
