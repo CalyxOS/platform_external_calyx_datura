fb925ab93b349982e1642dc58239e56ae7fd3e5e
Aayush Gupta <aayushgupta219@gmail.com>
2024-09-02 15:48:23 +0530

    MainActivity: Enable and adjust for edge to edge support
    Android 15 will force all apps to run edge to edge
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I49f26a9066912d1747f9d5701b8d3cef61e5c065

--- app/src/main/java/org/calyxos/datura/main/MainActivity.kt
+++ app/src/main/java/org/calyxos/datura/main/MainActivity.kt
@@ -8,18 +8,33 @@ package org.calyxos.datura.main
 import android.os.Bundle
 import android.os.UserManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.AndroidEntryPoint
 import org.calyxos.datura.R
+import org.calyxos.datura.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         DynamicColors.applyToActivityIfAvailable(this)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Adjust root view's paddings for edgeToEdge display
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { root, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            root.setPadding(insets.left, insets.top, insets.right, 0)
+            WindowInsetsCompat.CONSUMED
+        }
+
+        setContentView(binding.root)
 
         if (getSystemService(UserManager::class.java)?.isManagedProfile == true) {
             navigateToWorkFragment()
