f804426a5fc32db78dfce1caa514cbf71783ea64
Aayush Gupta <aayushgupta219@gmail.com>
2025-01-24 12:54:40 +0700

    Enable java 21 language features
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ifbfe309ba4b1dd0867aaf871a423d0d374ad5fb2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,10 @@ plugins {
     alias(libs.plugins.hilt.android.plugin)
 }
 
+kotlin {
+    jvmToolchain(21)
+}
+
 android {
     namespace = "org.calyxos.datura"
     compileSdk = 35
@@ -51,11 +55,11 @@ android {
         viewBinding = true
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
     lint {
         lintConfig = file("lint.xml")
